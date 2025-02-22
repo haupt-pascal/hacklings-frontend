4cc683079107c8796533897c5b8657a2baafd986
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2025-02-22 13:51:47 +0100

    added packages, added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+  },
+  css: ["~/assets/css/var.css"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/color-mode"],
+  icon: {
+    collections: ["simple-icons"],
+  },
+  colorMode: {
+    classSuffix: "",
+    fallback: "winter",
+    preference: "winter",
+    dataValue: "theme",
+    storageKey: "nuxt-color-mode",
+  },
+});
